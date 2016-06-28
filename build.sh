ee7072a2d188524440439ff8d1f63c7b4a7b2223
Vadim Rutkovsky <vrutkovs@redhat.com>
2016-06-28 02:17:09 +0200

    Use correct name matching application ID
    This also adds a patch to change name to a valid Dbus path

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 set -ex
 
 rm -rf app
-flatpak-builder --ccache --require-changes --repo=repo --subject="Nightly build of Something For Reddit, `date`" app com.github.samdroid_apps.something_for_reddit.json
+flatpak-builder --ccache --require-changes --repo=repo --subject="Nightly build of Something For Reddit, `date`" app today.sam.reddit_is_gtk.json
 
 flatpak build-update-repo --prune --prune-depth=20 repo
 
 # The following commands should be performed once
 flatpak --user remote-add --no-gpg-verify nightly-sfr ./repo || true
-flatpak --user -v install nightly-sfr com.github.samdroid_apps.something_for_reddit || true
+flatpak --user install nightly-sfr today.sam.reddit_is_gtk || true
 
-flatpak update --user com.github.samdroid_apps.something_for_reddit
+flatpak update --user today.sam.reddit_is_gtk
